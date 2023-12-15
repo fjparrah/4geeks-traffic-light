59d42402d79a2ce315394dc472588ccfa19f66a7
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-15 00:16:59 +0000

    actualizacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import green from "../../img/Green-circle.png";
+import red from "../../img/Red_Circle.png";
+import yellow from "../../img/Yellow_circle.png";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="containers py-5">
+			<div className="row justify-content-center">
+				    <div className="col-1">
+						<div className="row justify-content-center">
+							<div className="col-1 bg-dark text-dark">
+									
+									<h3>1</h3>
+									<h3>1</h3>
+							</div>
+							<div className="bg-dark text-light text-center" >
+							<img src={green} width="100" height="100" />
+							<img className="mb-1"src={red} width="90" height="90" />
+							<img className="mt-1 mb-2" src={yellow} width="90" height="90" />
+							
+						
+							</div>
+							</div>
+					</div>
+			</div>
 		</div>
+		</>
 	);
 };
 
