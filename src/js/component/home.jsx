68fe01585f7cccd91af698944e84c08b9a837e74
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-15 00:29:01 +0000

    actualizacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,36 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
+// Include images into your bundle
 import green from "../../img/Green-circle.png";
 import red from "../../img/Red_Circle.png";
 import yellow from "../../img/Yellow_circle.png";
 
-//create your first component
+// Create your first component
 const Home = () => {
-	return (
-		<>
-		<div className="containers py-5">
-			<div className="row justify-content-center">
-				    <div className="col-1">
-						<div className="row justify-content-center">
-							<div className="col-1 bg-dark text-dark">
-									
-									<h3>1</h3>
-									<h3>1</h3>
-							</div>
-							<div className="bg-dark text-light text-center" >
-							<img src={green} width="100" height="100" />
-							<img className="mb-1"src={red} width="90" height="90" />
-							<img className="mt-1 mb-2" src={yellow} width="90" height="90" />
-							
-						
-							</div>
-							</div>
-					</div>
-			</div>
-		</div>
-		</>
-	);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+  };
+
+  return (
+    <>
+      <div className="containers py-5">
+        <div className="row justify-content-center">
+          <div className="col-1">
+            <div className="row justify-content-center">
+              <div className="col-1 bg-dark text-dark">
+                <h3>1</h3>
+                <h3>1</h3>
+              </div>
+              <div className="bg-dark text-light text-center">
+                <img
+                  src={green}
+                  width="100"
+                  height="100"
+                  onClick={() => handleImageClick(green)}
+                  style={{
+                    filter:
+                      selectedImage === green ? "brightness(500%)" : "none",
+                  }}
+                />
+                <img
+                  className="mb-1"
+                  src={red}
+                  width="90"
+                  height="90"
+                  onClick={() => handleImageClick(red)}
+                  style={{
+                    filter: selectedImage === red ? "brightness(500%)" : "none",
+                  }}
+                />
+                <img
+                  className="mt-1 mb-2"
+                  src={yellow}
+                  width="90"
+                  height="90"
+                  onClick={() => handleImageClick(yellow)}
+                  style={{
+                    filter:
+                      selectedImage === yellow ? "brightness(500%)" : "none",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
